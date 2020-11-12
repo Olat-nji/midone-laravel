d5c586eaf8bf1a462d36d7e87d23d80738ae4185
Olatunji Olayemi <olayemi289@gmail.com>
2020-11-12 16:12:31 +0100

    la

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,11 +15,11 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 8);
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            // $table->morphs('tokenable');
+            // $table->string('name');
+            // $table->string('token', 8);
+            // $table->text('abilities')->nullable();
+            // $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
         });
     }
