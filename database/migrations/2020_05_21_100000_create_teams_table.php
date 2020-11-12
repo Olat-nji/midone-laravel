0df0607dd5f3ade6751a436c33646702541becdb
Olatunji Olayemi <olayemi289@gmail.com>
2020-11-12 16:02:47 +0100

    ka

--- database/migrations/2020_05_21_100000_create_teams_table.php
+++ database/migrations/2020_05_21_100000_create_teams_table.php
@@ -15,7 +15,7 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->index();
             $table->string('name');
             $table->boolean('personal_team');
             $table->timestamps();
