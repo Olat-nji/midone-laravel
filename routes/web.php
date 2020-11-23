ae9e23d0ae2f281fd4a4ee306dcab1438675c237
Olatunji Olayemi <olayemi289@gmail.com>
2020-11-23 08:22:55 +0100

    wire target edited

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('about', App\Http\Main\About::class)->name('about');
 Route::get('pricing', App\Http\Main\Pricing::class)->name('pricing');
 Route::get('portfolio', App\Http\Main\Portfolio::class)->name('portfolio');
 Route::get('contact', App\Http\Main\Contact::class)->name('contact');
-Route::post('git-pull-254928298376839', function(){
+Route::get('git-pull-it', function(){
     return shell_exec('git pull origin new-frontend-main');
     
 });
